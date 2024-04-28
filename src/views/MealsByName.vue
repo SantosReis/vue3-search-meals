8c1cb9dbc2e341609e8e3a00fb43da127b8a796b
Marcus Reis <mvsantosreis@gmail.com>
2024-04-28 18:05:55 +0100

    Search meal VueX cycle

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>SearchByName</div>
+  <div class="flex p-8 flex-col items-center justify-center">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 border-gray-200 w-72"
+      placeholder="Search for Meals"
+      @change="searchMeals"
+    />
+  </div>
+  <pre>{{ meals }}</pre>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from '@vue/reactivity';
+import { ref } from 'vue';
+import store from '../store';
+
+const keyword = ref('');
+const meals = computed(() => store.state.searchedMeals);
+
+function searchMeals() {
+  store.dispatch('searchMeals', keyword.value);
+}
+</script>
