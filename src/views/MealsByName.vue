119dde08f65eeed724197e904f4a741dd4dc8a42
Marcus Reis <mvsantosreis@gmail.com>
2024-04-28 20:10:55 +0100

    Retrieving data from Meal Details

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -15,11 +15,13 @@
       :key="meal.idMeal"
       class="bg-white shadow rounded-xl"
     >
-      <img
-        :src="meal.strMealThumb"
-        :alt="strMeal"
-        class="rounded-t-xl w-full h-48 object-cover"
-      />
+      <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
+        <img
+          :src="meal.strMealThumb"
+          :alt="meal.strMeal"
+          class="rounded-t-xl w-full h-48 object-cover"
+        />
+      </router-link>
       <div class="p-3">
         <h3 class="font-bold">{{ meal.strMeal }}</h3>
         <p class="mb-4">
