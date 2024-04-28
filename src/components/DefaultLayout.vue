a0451ee1d5501e338280a9789e6f46fc1352462d
Marcus Reis <mvsantosreis@gmail.com>
2024-04-28 14:34:25 +0100

    Set menu into Nav Component

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-gray-100 h-full">
+    <Navbar />
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Navbar from './Navbar.vue';
+
+console.log('DefaultLayout.vue');
+</script>
+
+<style scoped></style>
