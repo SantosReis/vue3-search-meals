a55a2f1ebd361c8cfd13fb6064e1c177b18d1915
Marcus Reis <mvsantosreis@gmail.com>
2024-04-28 11:41:25 +0100

    Search by Letter Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,30 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div class="flex p-8 flex-col items-center justify-center">
+    <div>
+      <input
+        type="text"
+        class="rounded border-2 border-gray-200 w-72"
+        placeholder="Search for Meals"
+      />
+    </div>
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters.split('')"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
 // import { computed } from 'vuex/types/helpers.js';
 // import { computed } from '@vue/reactivity';
 import { computed } from 'vue';
-import store from './../store'
+import store from './../store';
 
-const meals = computed(() => store.state.meals)
+const meals = computed(() => store.state.meals);
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 </script>
