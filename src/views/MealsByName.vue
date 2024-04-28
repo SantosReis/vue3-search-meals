0d56e355dab0daf5306166c2bfd08351091b023f
Marcus Reis <mvsantosreis@gmail.com>
2024-04-28 19:22:22 +0100

    Styling Meal Thumbnails

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -8,7 +8,43 @@
       @change="searchMeals"
     />
   </div>
-  <pre>{{ meals }}</pre>
+
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <div
+      v-for="meal of meals"
+      :key="meal.idMeal"
+      class="bg-white shadow rounded-xl"
+    >
+      <img
+        :src="meal.strMealThumb"
+        :alt="strMeal"
+        class="rounded-t-xl w-full h-48 object-cover"
+      />
+      <div class="p-3">
+        <h3 class="font-bold">{{ meal.strMeal }}</h3>
+        <p class="mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in
+          magnam reprehenderit dolores natus assumenda esse possimus sunt
+          voluptatem nemo, perspiciatis alias quos corrupti voluptates minus
+          veritatis incidunt vero voluptatum.
+        </p>
+        <div class="flex items-center justify-between">
+          <a
+            :href="meal.strYoutube"
+            target="_blank"
+            class="px-3 py-2 rounded border-2 text-white border-orange-600 bg-orange-500 hover:bg-orange-600 transition-colors"
+            >Youtube</a
+          >
+          <router-link
+            to="/"
+            class="ml-3 px-3 py-2 rounded border-2 border-transparent text-indigo-600 transition-colors"
+          >
+            View
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
