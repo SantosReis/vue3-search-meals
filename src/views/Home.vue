b9937f117c2eec121fcd86cd1bcf674a5c531855
Marcus Reis <mvsantosreis@gmail.com>
2024-04-28 06:20:45 +0100

    Install Vuex and create store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
-  Home
+  <pre>{{ meals }}</pre>
 </template>
 
 <script setup>
+// import { computed } from 'vuex/types/helpers.js';
+// import { computed } from '@vue/reactivity';
+import { computed } from 'vue';
+import store from './../store'
 
+const meals = computed(() => store.state.meals)
 </script>
