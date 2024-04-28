2c3ca59d2b673557c729e8a0eda69e7be59bea22
Marcus Reis <mvsantosreis@gmail.com>
2024-04-28 19:34:37 +0100

    Sync search means with url

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -49,13 +49,22 @@
 
 <script setup>
 import { computed } from '@vue/reactivity';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import store from '../store';
+import { useRoute } from 'vue-router';
 
+const route = useRoute();
 const keyword = ref('');
 const meals = computed(() => store.state.searchedMeals);
 
 function searchMeals() {
   store.dispatch('searchMeals', keyword.value);
 }
+
+onMounted(() => {
+  keyword.value = route.params.name;
+  if (keyword.value) {
+    searchMeals();
+  }
+});
 </script>
