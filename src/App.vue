9e99cf158497974800774492372f06730f6384f2
Marcus Reis <mvsantosreis@gmail.com>
2024-04-28 12:49:12 +0100

    Main Menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="bg-gray-100 h-full">
-    <header class="bg-white shadow h-16"></header>
+    <header class="bg-white shadow h-16 flex justify-between items-stretch">
+      <router-link
+        :to="{ name: 'home' }"
+        class="inline-flex items-center h-full px-5"
+      >
+        Home
+      </router-link>
+      <div class="flex items-center gap-1">
+        <router-link
+          to="/"
+          class="inline-block items-center px-2 h-full transition-colors hover:bg-purple-200"
+          >Search Meals</router-link
+        >
+        <router-link
+          to="/"
+          class="inline-block items-center px-2 h-full transition-colors hover:bg-purple-200"
+          >Search By Letter</router-link
+        >
+        <router-link
+          to="/"
+          class="inline-block items-center px-2 h-full transition-colors hover:bg-purple-200"
+          >Search By ingredients</router-link
+        >
+      </div>
+    </header>
     <main>
       <router-view />
     </main>
