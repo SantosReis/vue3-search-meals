5ccd9702ecdf9e1e0e1c19ae145bf60f83824226
Marcus Reis <mvsantosreis@gmail.com>
2024-04-29 19:00:05 +0100

    Meal Item compoment

---
+++ src/components/MealItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bg-white shadow rounded-xl">
+    <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
+      <img
+        :src="meal.strMealThumb"
+        :alt="meal.strMeal"
+        class="rounded-t-xl w-full h-48 object-cover"
+      />
+    </router-link>
+    <div class="p-3">
+      <h3 class="font-bold">{{ meal.strMeal }}</h3>
+      <p class="mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in magnam
+        reprehenderit dolores natus assumenda esse possimus sunt voluptatem
+        nemo, perspiciatis alias quos corrupti voluptates minus veritatis
+        incidunt vero voluptatum.
+      </p>
+      <div class="flex items-center justify-between">
+        <YouTubeButton :href="meal.strYoutube" />
+        <router-link
+          to="/"
+          class="ml-3 px-3 py-2 rounded border-2 border-transparent text-indigo-600 transition-colors"
+        >
+          View
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import YouTubeButton from '../components/YouTubeButton.vue';
+
+const { meal } = defineProps({
+  meal: {
+    required: true,
+    type: Object,
+  },
+});
+</script>
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -10,37 +10,7 @@
   </div>
 
   <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <div
-      v-for="meal of meals"
-      :key="meal.idMeal"
-      class="bg-white shadow rounded-xl"
-    >
-      <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
-        <img
-          :src="meal.strMealThumb"
-          :alt="meal.strMeal"
-          class="rounded-t-xl w-full h-48 object-cover"
-        />
-      </router-link>
-      <div class="p-3">
-        <h3 class="font-bold">{{ meal.strMeal }}</h3>
-        <p class="mb-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in
-          magnam reprehenderit dolores natus assumenda esse possimus sunt
-          voluptatem nemo, perspiciatis alias quos corrupti voluptates minus
-          veritatis incidunt vero voluptatum.
-        </p>
-        <div class="flex items-center justify-between">
-          <YouTubeButton :href="meal.strYoutube" />
-          <router-link
-            to="/"
-            class="ml-3 px-3 py-2 rounded border-2 border-transparent text-indigo-600 transition-colors"
-          >
-            View
-          </router-link>
-        </div>
-      </div>
-    </div>
+    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
 </template>
 
@@ -49,7 +19,7 @@ import { computed } from '@vue/reactivity';
 import { onMounted, ref } from 'vue';
 import store from '../store';
 import { useRoute } from 'vue-router';
-import YouTubeButton from '../components/YouTubeButton.vue';
+import MealItem from '../components/MealItem.vue';
 
 const route = useRoute();
 const keyword = ref('');
